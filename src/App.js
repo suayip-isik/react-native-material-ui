4f589307a13c364e05f05d7096469b7989782c19
Şuayip Işık <suayip.isik.16@gmail.com>
2023-01-21 11:36:21 +0300

    @react-native-material/core and react-native-paper added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
+import { Button, ActivityIndicator, Stack } from "@react-native-material/core";
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>App</Text>
+      <Button title="Click Me" />
+
+      <Stack center spacing={4}>
+        <ActivityIndicator />
+        <ActivityIndicator size="large" />
+        <ActivityIndicator size="small" color="error" />
+        <ActivityIndicator size="large" color="#00ff00" />
+      </Stack>
     </SafeAreaView>
   )
 }
